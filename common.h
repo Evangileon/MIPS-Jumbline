4a0a57dd7dad3fd5631a698f7708de74dd280312
Eageon <yujun.eageon@gmail.com>
2014-03-06 17:15:55 -0600

    c

---
+++ common.h
@@ -0,0 +1,29 @@
+#ifndef __COMMON_JUMBLINE_RJ__
+#define __COMMON_JUMBLINE_RJ__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <strings.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char byte;
+typedef unsigned short ushort;
+typedef unsigned long ulong;
+typedef unsigned int uint;
+
+#ifdef __DEBUG__
+        #define DEBUG(format, ...) do { fprintf(stderr, format, ##__VA_ARGS__); }while(0)
+#else
+        #define DEBUG(format, ...) do { }while(0)
+#endif
+
+#define ERROR(format, ...) do { fprintf(stderr, format, ##__VA_ARGS__); exit(-1); }while(0)
+
+#endif
---
+++ rearrange.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+int rearrange(char* str, int n) {
+	
+	return 0;
+}
+
+
+int main(int argc, char** argv) {
+	if(argc < 2) {
+		
+	}	
+
+	return 0;
+}
