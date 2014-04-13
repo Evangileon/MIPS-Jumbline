f13f496731a81a3c266b62d70e1c9f60cdc63075
TexasRed <huangkai1988@gmail.com>
2014-04-12 22:19:43 -0500

    Update Kai_macros.asm

--- Kai_macros.asm
+++ Kai_macros.asm
@@ -242,15 +242,15 @@ QUIT:
 print_str("Sorry, wrong guess!")
 li $v0,0
 incorrect:
-sw 	%candidate,($sp)
-sw 	%length,4($sp)
-sw 	$s0, 8($sp)
-sw	$s1, 12($sp)
-sw	$s2, 16($sp)
-sw 	$s3, 20($sp)
-sw	$s4, 24($sp)
-sw	$s5, 28($sp)
-sw	$s6, 32($sp)
+lw 	%candidate,($sp)
+lw 	%length,4($sp)
+lw 	$s0, 8($sp)
+lw	$s1, 12($sp)
+lw	$s2, 16($sp)
+lw 	$s3, 20($sp)
+lw	$s4, 24($sp)
+lw	$s5, 28($sp)
+lw	$s6, 32($sp)
 addi 	$sp,$sp, 36
 .end_macro 
 ################################################################3
